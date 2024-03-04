8daabab8b92b778ce7a5fb149997759538be558d
Maxime Héliot <maxime.heliotpro@gmail.com>
2024-03-04 23:00:10 +0100

    Add Procedural Grid lesson

---
+++ Assets/Procedural Grid/Scripts/Grid.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Generate a simple rectangular grid
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Grid : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    private Vector3[] vertices;
+
+    private Mesh mesh;
+    
+    private void Awake()
+    {
+        Generate();
+    }
+    
+    private void Generate()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.05f);
+
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+        
+        // Initialize vertices array and position them
+        vertices = new Vector3[(width + 1) * (height + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+        for (int i = 0, y = 0; y <= height; y++)
+        {
+            for (int x = 0; x <= width; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+                uv[i] = new Vector2((float)x / width, (float)y / height);
+                tangents[i] = tangent;
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
+        
+        // Initialize triangles array
+        // Three consecutive vertices indices form a triangle
+        // Which side a triangle is visible from is determined by the orientation of its vertex indices.
+        // Clockwise direction is considered to be forward-facing and visible. Counter-clockwise triangles are discarded.
+        int[] triangles = new int[width * height * 6];
+        for (int ti = 0, vi = 0, y = 0; y < height; y++, vi++)
+        {
+            for (int x = 0; x < width; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + width + 1;
+                triangles[ti + 5] = vi + width + 2;
+            }
+        }
+        mesh.triangles = triangles;
+        
+        mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+            return;
+        
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+}
